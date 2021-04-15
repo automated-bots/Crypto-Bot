521d33f3c1ac58e1dc039e38ce1e6c304b44c4ed
Melroy van den Berg <melroy@melroy.org>
2021-04-15 21:02:10 +0000

    Update fetcher.js

--- src/fetcher.js
+++ src/fetcher.js
@@ -40,7 +40,7 @@ class Fetcher {
         interval: exchangeParams.interval,
         outputsize: exchangeParams.outputsize,
         order: 'ASC',
-        apikey: this.exchangeSettings.apiKey
+        apikey: this.exchangeSettings.api_token
       }
       // Both data arrays should contain data bout OHLC and datetime
       try {
