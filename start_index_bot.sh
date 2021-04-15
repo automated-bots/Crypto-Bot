91c75f95232c07ca93ba13779e2da41a008e2149
Melroy van den Berg <melroy@melroy.org>
2021-04-15 20:56:03 +0000

    Update start_index_bot.sh

--- start_index_bot.sh
+++ start_index_bot.sh
@@ -1,2 +1,2 @@
 #!/usr/bin/env bash
-docker run -p 3008:3008 --restart=always -v $(pwd)/config.yml:/app/config.yml -d danger89/index-bot
+docker run -p 3010:3010 --restart=always -v $(pwd)/config.yml:/app/config.yml -d danger89/crypto-bot
